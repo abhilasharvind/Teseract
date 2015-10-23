c1a6a0ce6c1ec65927957a958eec14cc600fba67
Abx <arvindabhilash@gmail.com>
2015-10-23 23:29:56 +0530

    renaming by  abx

--- src/com/jsservey/view/home/MainActivity.java
+++ src/com/jsservey/view/home/MainActivity.java
@@ -8,14 +8,22 @@ import com.jsservey.view.BaseActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements OnClickListener{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		Log.d("abx","in Main Activity");
-		setContentView(R.layout.survey_home_layout);
+		setContentView(R.layout.home_layout);
 		handleHomeClick(this.findViewById(android.R.id.content));
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
