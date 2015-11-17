a0186cf8fed7b00ab719471e029c40ee50089546
Arun Babu <arrruuun@gmail.com>
2015-11-17 21:56:20 +0530

    Arb :
    Question answer layout

--- src/com/jsservey/view/home/survey/StartSurveyActivity.java
+++ src/com/jsservey/view/home/survey/StartSurveyActivity.java
@@ -16,9 +16,9 @@ public class StartSurveyActivity extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.startsurvey_layout);
-		TextView prf_name = (TextView) findViewById(R.id.profile_name_tv);
-		AppPref appPref = new AppPref(getApplicationContext());
-		prf_name.setText(""+appPref.getString(PrefConstant.ACTIVATED_PROFILE));
+//		TextView prf_name = (TextView) findViewById(R.id.profile_name_tv);
+//		AppPref appPref = new AppPref(getApplicationContext());
+//		prf_name.setText(""+appPref.getString(PrefConstant.ACTIVATED_PROFILE));
 		Utility.startActivity(StartSurveyActivity.this, CreateQuestionTextActivity.class);
 	}
 
--- src/com/jsservey/view/home/survey/questions/QuestionsDisplayActivity.java
+++ src/com/jsservey/view/home/survey/questions/QuestionsDisplayActivity.java
@@ -4,12 +4,21 @@ import com.abx.jsservey.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class QuestionsDisplayActivity extends Activity{
+	private View answerView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.question_display_layout);
+		LinearLayout questionLayout = (LinearLayout) findViewById(R.id.answer_type_layout);
+		answerView = getLayoutInflater()
+				.inflate(R.layout.question_type_text_layout,
+						questionLayout, false);
+		questionLayout.removeAllViews();
+		questionLayout.addView(answerView);
 	}
 
 }
